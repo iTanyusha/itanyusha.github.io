e89d510e9943f9fd24169f1abbc93ad30a65ffc7
Tatiana Shibaeva <tatiana.shibaeva@abbyy.com>
2021-09-11 01:32:57 +0300

    test-cameras ios navigator.mediaDevices.getUserMedia

--- camera/script.js
+++ camera/script.js
@@ -1,24 +1,24 @@
-const imageInput = document.getElementById('image');
+// const imageInput = document.getElementById('image');
 const environmentInput = document.getElementById('environment');
-const userInput = document.getElementById('user');
+// const userInput = document.getElementById('user');
 const output = document.getElementById('output');
 const imRes = document.getElementById('imRes');
-const envButton = document.getElementById('envButton');
+// const envButton = document.getElementById('envButton');
 
-imageInput.addEventListener('change', (e) => processFiles(e.target.files));
+// imageInput.addEventListener('change', (e) => processFiles(e.target.files));
 
 environmentInput.addEventListener('change', (e) =>
   processFiles(e.target.files)
 );
 
-userInput.addEventListener('change', (e) => processFiles(e.target.files));
+// userInput.addEventListener('change', (e) => processFiles(e.target.files));
 
-envButton.addEventListener('click', () =>
-  window.setTimeout(() => {
-    // environmentInput.click(),
-    $('#environment').trigger('click');
-  }, 0)
-);
+// envButton.addEventListener('click', () =>
+//   window.setTimeout(() => {
+//     // environmentInput.click(),
+//     $('#environment').trigger('click');
+//   }, 0)
+// );
 
 var processFiles = (files) => {
   const url = URL.createObjectURL(files[0]);
@@ -26,20 +26,38 @@ var processFiles = (files) => {
   imRes.src = url;
 };
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   console.log('load');
-  // environmentInput.focus();
-  // window.setTimeout(() => environmentInput.click(), 100);
+
+  try {
+    await navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(async (stream) => {
+        await navigator.mediaDevices
+          .enumerateDevices()
+          .then((devices) =>
+            devices.filter((device) => device.kind === 'videoinput')
+          );
+
+        stream.getTracks().forEach((track) => track.stop());
+      });
+    window.console.log('camera on');
+    environmentInput.focus();
+    window.setTimeout(() => environmentInput.click(), 100);
+  } catch (e) {
+    window.console.log('camera off', e);
+  }
+
   // envButton.click(); //.dispatchEvent(new MouseEvent('click'));
   // $('#envButton').trigger('click');
 
-  $('body').append(
-    "<input id='aidCameraInput' type='file' capture='environment' accept='image/*'></input>"
-  );
-  $('#aidCameraInput').focus();
+  // $('body').append(
+  //   "<input id='aidCameraInput' type='file' capture='environment' accept='image/*'></input>"
+  // );
+  // $('#aidCameraInput').focus();
   // $('#aidCameraInput').click();
-  setTimeout(function () {
-    console.log('aaaa');
-    $('#aidCameraInput').click();
-  }, 1000);
+  // setTimeout(function () {
+  //   console.log('aaaa');
+  //   $('#aidCameraInput').click();
+  // }, 1000);
 });
